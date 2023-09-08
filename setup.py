9c9b17287f6579aa113f1338c08015d52e87cd4c
Belashov641618 <Uclaptrap@gmail.com>
2023-09-08 22:08:37 +0300

    Добавлены файлы для публикации библиотеки

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='matplotlib-simplifier',
+      version='1.0',
+      description='High level library to construct graphics with matplotlib',
+      packages=['matplotlib-simplifier'],
+      author_email='Uclaptrap@gmail.com',
+      zip_safe=False)
